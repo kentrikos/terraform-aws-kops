74c72b31b33c87d0fa35bb4ca8b873ac56af766d
Marek Zawadzki <marek.zawadzki@nordcloud.com>
2018-12-14 12:44:54 +0100

    Add support for exising AWS SSH keypairs

--- main.tf
+++ main.tf
@@ -58,6 +58,7 @@ locals {
   option_http_proxy                = "${var.http_proxy    != ""     ? "--http-proxy ${var.http_proxy}" : ""}"
   option_disable_natgw             = "${var.disable_natgw == "true" ? "--disable-natgw" : ""}"
   option_assume_cross_account_role = "${var.iam_cross_account_role_arn != "" ? "--assume-cross-account-role ${var.iam_cross_account_role_arn}" : ""}"
+  option_aws_ssh_keypair_name      = "${var.aws_ssh_keypair_name != "" ? "--ssh-keypair-name ${var.aws_ssh_keypair_name}" : ""}"
 }
 
 resource "null_resource" "kubernetes_cluster" {
@@ -81,6 +82,7 @@ resource "null_resource" "kubernetes_cluster" {
       ${local.option_assume_cross_account_role} \
       ${local.option_http_proxy} \
       ${local.option_disable_natgw}
+      ${local.option_aws_ssh_keypair_name}
 EOT
 
     working_dir = "kops"
